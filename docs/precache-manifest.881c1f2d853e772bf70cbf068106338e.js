ce9a8bed6337443ef44e5c73e8fecce49ac3876d
itsRicmor <ricardojms1999@gmail.com>
2020-12-26 19:21:34 -0600

    docs agregado

---
+++ docs/precache-manifest.881c1f2d853e772bf70cbf068106338e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dcc172a5c5f9128c9110003031f52d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1d5c2d7276f861ee86f",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d5c2d7276f861ee86f",
+    "url": "/static/js/main.4d931ae2.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
